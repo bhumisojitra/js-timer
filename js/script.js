1381f46ca1f6a8a1b08a5ef1aa0dc5f179ad7d4d
bhumisojitra <bhumisojitra129@gmail.com>
2024-07-10 20:52:41 +0530

    timer

---
+++ js/script.js
@@ -0,0 +1,64 @@
+let day = document.getElementById('day');
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let second = document.getElementById('second');
+
+const timer = () => {
+
+    let days = 0;
+    let hr = 0;
+    let min = 0;
+    let sec = 0;
+
+    day.innerHTML = days;
+    hour.innerHTML = hr;
+    minute.innerHTML = min;
+    second.innerHTML = sec;
+
+    setInterval(() => {
+        sec++;
+        second.innerHTML = sec;
+
+        if(days <= 9){
+            day.innerHTML = "0" + days;
+        }
+        if(hr <= 9){
+            hour.innerHTML = "0" + hr;
+        }
+        if(min <= 9){
+            minute.innerHTML = "0" + min;
+        }
+        if(sec <= 9){
+            second.innerHTML = "0" + sec;
+        }
+
+        if(sec >= 60){
+            min++;
+            sec = 0;
+            second.innerHTML = sec++;
+            minute.innerHTML = min++;
+        }
+
+        if(min > 59){
+            hr++;
+            min = 0;
+            second.innerHTML = sec++;
+            minute.innerHTML = min++;
+            hour.innerHTML = hr++;
+        }
+
+        if(hr > 23){
+            days++;
+            hr = 0;
+            second.innerHTML = sec++;
+            minute.innerHTML = min++;
+            hour.innerHTML = hr++;
+            day.innerHTML = days++;
+        }
+
+    }, 1000)
+}
+window.onload = function() {
+    alert("please click and start timer");
+    timer();
+};
